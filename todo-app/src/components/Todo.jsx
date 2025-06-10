3563807decea35cb49f729f61d67b7e8ba8ac603
김승혁 <peter9889466@gmail.com>
2025-06-10 15:42:06 +0900

    MoveCommit

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,19 +1,21 @@
 import React, { useRef, useState } from 'react'
+import BottomMove from './BottomMove';
+import TopMove from './TopMove';
 
 const Todo = () => {
 
     //Todo
     //- 사용자가 등록한 업무를 보관할 배열 생성(state)
-    const [todos, setTodos]=useState([]);
+    const [todos, setTodos] = useState([]);
     const inputRef = useRef("");
-    const addTodo =(event) =>{
+    const addTodo = (event) => {
         event.preventDefault();
         let todo = inputRef.current.value;
         // todos.push(todo);
         // setTodos(todos);
         setTodos(todos.concat(todo))
         console.log(todos);
-        
+
     }
 
     return (
@@ -21,13 +23,18 @@ const Todo = () => {
             <h1>Todo List</h1>
             <div>
                 <form action="">
-                    <input type="text" name="" id="" ref={inputRef}/>
-                    <input type="submit" name="" id="" value="등록" onClick={addTodo}/>
+                    <input type="text" name="" id="" ref={inputRef} />
+                    <input type="submit" name="" id="" value="등록" onClick={addTodo} />
                 </form>
             </div>
             <div>
-                {todos.map((item,index)=>(
-                    <p key={index}>  {index+1}. <input type='checkbox'/>{item}<button>삭제</button><button>내용 수정하기</button><button>맨 맽으로</button><button>맨 위로</button></p>
+                {todos.map((item, index) => (
+                    <p key={index}>  {index + 1}. <input type='checkbox' />{item}
+                        <button>삭제</button>
+                        <button>내용 수정하기</button>
+                        <BottomMove index={index} todos={todos} setTodos={setTodos} />
+                        <TopMove index={index} todos={todos} setTodos={setTodos} />
+                    </p>
                 ))}
             </div>
         </div>
