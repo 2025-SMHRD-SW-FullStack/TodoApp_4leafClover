d7a9d8a37a9072bf698855c7f649a3d8a1e3e3f3
김희광 <gmlrhkd432@gmail.com>
2025-06-10 16:08:40 +0900

    삭제하기 기능

--- todo-app/src/components/Todo.jsx
+++ todo-app/src/components/Todo.jsx
@@ -1,37 +1,46 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from 'react';
+import Delete from './Delete'; 
 
 const Todo = () => {
+    
+  //Todo
+  //- 사용자가 등록한 업무를 보관할 배열 생성(state)
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef("");
 
-    //Todo
-    //- 사용자가 등록한 업무를 보관할 배열 생성(state)
-    const [todos, setTodos]=useState([]);
-    const inputRef = useRef("");
-    const addTodo =(event) =>{
-        event.preventDefault();
-        let todo = inputRef.current.value;
-        // todos.push(todo);
-        // setTodos(todos);
-        setTodos(todos.concat(todo))
-        console.log(todos);
-        
+  const addTodo = (event) => {
+    event.preventDefault(); 
+    let todo = inputRef.current.value.trim(); 
+
+    if (todo) { 
+      setTodos(todos.concat(todo));
+      inputRef.current.value = ""; 
     }
+    console.log(todos);
+  };
 
-    return (
-        <div>
-            <h1>Todo List</h1>
-            <div>
-                <form action="">
-                    <input type="text" name="" id="" ref={inputRef}/>
-                    <input type="submit" name="" id="" value="등록" onClick={addTodo}/>
-                </form>
-            </div>
-            <div>
-                {todos.map((item,index)=>(
-                    <p key={index}>  {index+1}. <input type='checkbox'/>{item}<button>삭제</button><button>내용 수정하기</button><button>맨 맽으로</button><button>맨 위로</button></p>
-                ))}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <div>
+        <form onSubmit={addTodo}> 
+          <input type="text" ref={inputRef} placeholder="새로운 할 일을 입력하세요"/>
+          <input type="submit" value="등록"/> 
+        </form>
+      </div>
+      <div>
+        {todos.map((item, index) => (
+          <p key={index}>
+            {index + 1}. <input type='checkbox'/> {item}
+            <Delete todos={todos} setTodos={setTodos} index={index} />
+            <button>내용 수정하기</button>
+            <button>맨 밑으로</button>
+            <button>맨 위로</button>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Todo
+export default Todo;
