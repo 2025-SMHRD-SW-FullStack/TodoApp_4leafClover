a05a2095258dc1da60448d1ab6ad38ac6f13d563
김승혁 <peter9889466@gmail.com>
2025-06-10 15:48:53 +0900

    TopBottom

--- todo-app/src/components/BottomMove.jsx
+++ todo-app/src/components/BottomMove.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const BottomMove = ({ index, todos, setTodos }) => {
     const moveToBottom = () => {
-        if (index === todos.length - 1) return; // 이미 맨 아래
+        if (index === todos.length - 1) return;
         const item = todos[index];
         const newTodos = [...todos.filter((_, i) => i !== index), item];
         setTodos(newTodos);
     };
 
     return (
-        <button onClick={moveToBottom}>🔽 맨 아래</button>
+        <button onClick={moveToBottom}>맨 아래</button>
     );
 };
 
